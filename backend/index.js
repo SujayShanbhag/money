60b6ade6549adafd9ad8d928ade79fdb20cbbabc
SujayShanbhag <sujay.shanbhag999@gmail.com>
2024-01-22 18:22:44 +0530

    working backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
-const express = require("express");
+const express = require('express');
+const cors = require("cors");
+const app = express();
+const bodyparser = require("body-parser")
+const mainRouter = require("./router/index");
 
+
+app.use(cors());
+app.use(bodyparser.json());
+
+// Single routing
+app.use("/api/v1",mainRouter);
+
+app.listen(3000,() => console.log("running"));
 
